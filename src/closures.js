252b775d85cc83bdff5602de0dfdbe48e1246e61
OmarIsmail277 <omar.ismail277@gmail.com>
2026-05-25 19:03:27 +0300

    Implemented once function in closures.js

--- src/closures.js
+++ src/closures.js
@@ -3,6 +3,18 @@
 // 6. once
 // return a wrapper function that only executes fn the first time
 
+function once(fn) {
+  let called = false;
+
+  return function (...args) {
+    if (!called) {
+      called = true;
+      // return fn(...args);
+      return fn.apply(this, args);
+    }
+  };
+}
+
 // 7. memoize
 // return a wrapper that caches results by argument
 
