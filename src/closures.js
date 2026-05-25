dd5fe77f53538cc80929f55c98c953ccf2c6c8c0
OmarIsmail277 <omar.ismail277@gmail.com>
2026-05-25 19:34:07 +0300

    Implemented createCounter function in closures.js

--- src/closures.js
+++ src/closures.js
@@ -18,8 +18,38 @@ function once(fn) {
 // 7. memoize
 // return a wrapper that caches results by argument
 
+function memoize(fn) {
+  const cache = new Map();
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+
+    if (cache.has(key)) {
+      console.log("Cache HIT!");
+      return cache.get(key);
+    }
+    console.log("Cache MISS");
+    const result = fn.apply(this, args);
+    cache.set(key, result);
+    return result;
+  };
+}
+
+const squre = memoize((x) => x * x);
+
 // 8. createCounter
 // use closure to hold private count, return increment/decrement/reset
+
+function createCounter(action) {
+  let counter = 0;
+
+  return function (action) {
+    if (action === "increment") counter++;
+    if (action === "decrement") counter--;
+    if (action === "reset") counter = 0;
+    return counter;
+  };
+}
 
 // 9. createSecretHolder
 // use closure to hold private data, return getSecret/setSecret
