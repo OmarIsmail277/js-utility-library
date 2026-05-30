b08833d2a1668ae9470b45c6fa67f7dd9150c299
OmarIsmail277 <omar.ismail277@gmail.com>
2026-05-30 15:36:54 +0300

    asyncPipe Done

--- src/async.js
+++ src/async.js
@@ -2,3 +2,16 @@
 
 // 12. pipeAsync
 // take multiple async functions, pipe output of each into the next (left-to-right)
+
+const pipe =
+  (...fns) =>
+  (value) =>
+    fns.reduce((acc, fn) => fn(acc), value);
+
+const pipeAsync =
+  (...fns) =>
+  (value) =>
+    fns.reduce(async (accPromise, fn) => {
+      const acc = await accPromise;
+      return fn(acc);
+    }, Promise.resolve(value));
