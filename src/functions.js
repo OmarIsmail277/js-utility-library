03e9a8859066137f8001edd59f40efe8e25965ae
OmarIsmail277 <omar.ismail277@gmail.com>
2026-05-30 08:17:42 +0300

    implemented compose and deepClone custom functions in functions.js

--- src/functions.js
+++ src/functions.js
@@ -3,5 +3,23 @@
 // 10. compose
 // take multiple functions, return a new function that runs them right-to-left
 
+const compose = (...fns) => {
+  return function (value) {
+    return fns.reduceRight((acc, fn) => fn(acc), value);
+  };
+};
+
 // 11. deepClone
 // recursively clone objects and arrays without mutating the original
+
+function deepClone(obj) {
+  if (obj === null || typeof obj !== "object") return obj;
+
+  const clone = Array.isArray(obj) ? [] : {};
+
+  for (const key of Object.keys(obj)) {
+    clone[key] = deepClone(obj[key]);
+  }
+
+  return clone;
+}
